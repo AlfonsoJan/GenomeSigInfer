1a3bb25fadcccc5eff94f9fd3af04670a462410a
Jan Alfonso <alfonsobusker@gmail.com>
2023-10-20 10:53:43 +0200

    Added the results and code for cosine simi and jense shann dist

--- DeepBayesMutSig/DeepBayesMutSig/cosine.py
+++ DeepBayesMutSig/DeepBayesMutSig/cosine.py
@@ -12,6 +12,31 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import LabelEncoder
 
+def cosine_nmf_w_optimal(optimal_columns: dict, df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate the cosine similarity on the most optimal columns on (decompressed) nmf result.
+
+    Args:
+        df1 (pd.DataFrame): DataFrame containing signature data.
+        df2 (pd.DataFrame): DataFrame containing signature data.
+
+    Returns:
+        pd.DataFrame: And df with the cosine similarities
+    """
+    scaler = StandardScaler()
+    data1_scaled = scaler.fit_transform(df1.values)
+    data1_scaled = pd.DataFrame(data1_scaled, columns=df1.columns)
+
+    data2_scaled = scaler.transform(df2.values)
+    data2_scaled = pd.DataFrame(data2_scaled, columns=df2.columns)
+    row = {}
+    for col1, col2 in optimal_columns.items():
+        array1 = data1_scaled[col1].values.reshape(1, -1)
+        array2 = data2_scaled[col2].values.reshape(1, -1)
+        similarity = cosine_similarity(array1, array2, dense_output=False)
+        row[col1] = similarity[0, 0]
+    return row
+
 
 def most_similarity_decompose(filename: str) -> np.ndarray:
     """
